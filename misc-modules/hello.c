a4272fa604bdd7c436bc9f726652a34ac30f012c
Jessica McKellar <jesstess@mit.edu>
2013-12-23 20:17:13 -0500

    misc-modules: clean up hello.

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -1,17 +1,16 @@
-/*                                                     
- * $Id: hello.c,v 1.5 2004/10/26 03:32:21 corbet Exp $ 
- */                                                    
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/sched.h>
+
 MODULE_LICENSE("Dual BSD/GPL");
 
-static int hello_init(void)
+static int __init hello_init(void)
 {
 	printk(KERN_ALERT "Hello, world\n");
 	return 0;
 }
 
-static void hello_exit(void)
+static void __exit hello_exit(void)
 {
 	printk(KERN_ALERT "Goodbye, cruel world\n");
 }
