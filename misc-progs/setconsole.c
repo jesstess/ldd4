848298a13c2257b324d702f1b3d484d63b18b6b9
Jessica McKellar <jesstess@mit.edu>
2013-12-23 21:29:47 -0500

    misc-progs: clean up setconsole.

--- misc-progs/setconsole.c
+++ misc-progs/setconsole.c
@@ -27,14 +27,18 @@
 
 int main(int argc, char **argv)
 {
-    char bytes[2] = {11,0}; /* 11 is the TIOCLINUX cmd number */
+    /* 11 is the TIOCLINUX subcommand for specifying the console to receive
+       kernel messages. */
+    char bytes[2] = {11, 0};
 
-    if (argc==2) bytes[1] = atoi(argv[1]); /* the chosen console */
-    else {
-        fprintf(stderr, "%s: need a single arg\n",argv[0]); exit(1);
+    if (argc == 2) {
+        bytes[1] = atoi(argv[1]); /* The chosen console. */
+    } else {
+        fprintf(stderr, "%s: need a single arg\n", argv[0]);
+	exit(1);
     }
-    if (ioctl(STDIN_FILENO, TIOCLINUX, bytes)<0) {    /* use stdin */
-        fprintf(stderr,"%s: ioctl(stdin, TIOCLINUX): %s\n",
+    if (ioctl(STDIN_FILENO, TIOCLINUX, bytes) < 0) { /* Use stdin. */
+        fprintf(stderr, "%s: ioctl(stdin, TIOCLINUX): %s\n",
                 argv[0], strerror(errno));
         exit(1);
     }
