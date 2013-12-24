662dfaff62e300045fa8b27885a2c6623ad0f55e
Jessica McKellar <jesstess@mit.edu>
2013-12-23 21:03:58 -0500

    misc-progs: Clean up setlevel.

--- misc-progs/setlevel.c
+++ misc-progs/setlevel.c
@@ -22,25 +22,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-/* #include <unistd.h> */ /* conflicting on the alpha */
-#define __LIBRARY__ /* _syscall3 and friends are only available through this */
-#include <linux/unistd.h>
 
-/* define the system call, to override the library function */
-_syscall3(int, syslog, int, type, char *, bufp, int, len);
+/* A script to change the console log level. It takes as a command
+   line argument an integer between 1 and 8 inclusive. Higher numbers
+   mean more gets printed to the console. */
 
 int main(int argc, char **argv)
 {
     int level;
 
-    if (argc==2) {
-	level = atoi(argv[1]); /* the chosen console */
+    if (argc == 2) {
+	level = atoi(argv[1]);
     } else {
-        fprintf(stderr, "%s: need a single arg\n",argv[0]); exit(1);
+        fprintf(stderr, "%s: need a single arg\n", argv[0]);
+	exit(1);
     }
-    if (syslog(8,NULL,level) < 0) {  
-        fprintf(stderr,"%s: syslog(setlevel): %s\n",
-                argv[0],strerror(errno));
+    /* 8 says we are changing the log level. See the syslog(2) man page for the
+       full list of actions. */
+    if (klogctl(8, NULL, level) < 0) {  
+        fprintf(stderr,"%s: syslog(setlevel): %s\n", argv[0], strerror(errno));
         exit(1);
     }
     exit(0);
