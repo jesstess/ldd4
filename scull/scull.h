1ec4e7896616ca58ddcc67e51de31688254cbabe
Jessica McKellar <jesstess@mit.edu>
2013-12-23 21:52:00 -0500

    scull: clean up header.

--- scull/scull.h
+++ scull/scull.h
@@ -12,61 +12,65 @@
  * by O'Reilly & Associates.   No warranty is attached;
  * we cannot take responsibility for errors or fitness for use.
  *
- * $Id: scull.h,v 1.15 2004/11/04 17:51:18 rubini Exp $
  */
 
 #ifndef _SCULL_H_
 #define _SCULL_H_
 
-#include <linux/ioctl.h> /* needed for the _IOW etc stuff used later */
+/* Gives us access to ioctl macros _IO and friends below. */
+#include <linux/ioctl.h>
 
 /*
- * Macros to help debugging
+ * Macros to help with debugging. Set SCULL_DEBUG to 1 enable
+ * debugging (which you can do from the Makefile); these macros work
+ * in both kernelspace and userspace.
  */
 
-#undef PDEBUG             /* undef it, just in case */
+#undef PDEBUG /* undef it, just in case someone else defined it. */
 #ifdef SCULL_DEBUG
 #  ifdef __KERNEL__
-     /* This one if debugging is on, and kernel space */
-#    define PDEBUG(fmt, args...) printk( KERN_DEBUG "scull: " fmt, ## args)
+     /* Debugging is on and we are in kernelspace. */
+#    define PDEBUG(fmt, args...) printk(KERN_DEBUG "scull: " fmt, ## args)
 #  else
-     /* This one for user space */
+     /* Debugging is on and we are in userspace. */
 #    define PDEBUG(fmt, args...) fprintf(stderr, fmt, ## args)
 #  endif
 #else
-#  define PDEBUG(fmt, args...) /* not debugging: nothing */
+#  define PDEBUG(fmt, args...) /* Not debugging: do nothing. */
 #endif
 
+/* PDEBUGG is a placeholder that makes it easy to "comment out" the debugging
+   statements without deleting them. */
 #undef PDEBUGG
-#define PDEBUGG(fmt, args...) /* nothing: it's a placeholder */
+#define PDEBUGG(fmt, args...)
 
 #ifndef SCULL_MAJOR
-#define SCULL_MAJOR 0   /* dynamic major by default */
+#define SCULL_MAJOR     0 /* dynamic major by default */
 #endif
 
 #ifndef SCULL_NR_DEVS
-#define SCULL_NR_DEVS 4    /* scull0 through scull3 */
+#define SCULL_NR_DEVS   4 /* scull0 through scull3 */
 #endif
 
 #ifndef SCULL_P_NR_DEVS
-#define SCULL_P_NR_DEVS 4  /* scullpipe0 through scullpipe3 */
+#define SCULL_P_NR_DEVS 4 /* scullpipe0 through scullpipe3 */
 #endif
 
 /*
- * The bare device is a variable-length region of memory.
- * Use a linked list of indirect blocks.
+ * The bare device is a variable-length region of memory. Use a
+ * linked list of indirect blocks.
  *
- * "scull_dev->data" points to an array of pointers, each
- * pointer refers to a memory area of SCULL_QUANTUM bytes.
+ * "scull_dev->data" points to an array of pointers, each pointer
+ * refers to a memory area of SCULL_QUANTUM bytes.
  *
  * The array (quantum-set) is SCULL_QSET long.
  */
 #ifndef SCULL_QUANTUM
-#define SCULL_QUANTUM 4000
+#define SCULL_QUANTUM  4000
 #endif
 
 #ifndef SCULL_QSET
-#define SCULL_QSET    1000
+#define SCULL_QSET     1000
 #endif
 
 /*
@@ -85,20 +89,20 @@ struct scull_qset {
 };
 
 struct scull_dev {
-	struct scull_qset *data;  /* Pointer to first quantum set */
-	int quantum;              /* the current quantum size */
-	int qset;                 /* the current array size */
-	unsigned long size;       /* amount of data stored here */
-	unsigned int access_key;  /* used by sculluid and scullpriv */
-	struct semaphore sem;     /* mutual exclusion semaphore     */
-	struct cdev cdev;	  /* Char device structure		*/
+	struct scull_qset *data;  /* Pointer to first quantum set. */
+	int quantum;              /* The current quantum size. */
+	int qset;                 /* The current array size. */
+	unsigned long size;       /* Amount of data stored here. */
+	unsigned int access_key;  /* Used by sculluid and scullpriv. */
+	struct semaphore sem;     /* Mutual exclusion semaphore. */
+	struct cdev cdev;	  /* Char device structure. */
 };
 
 /*
  * Split minors in two parts
  */
 #define TYPE(minor)	(((minor) >> 4) & 0xf)	/* high nibble */
-#define NUM(minor)	((minor) & 0xf)		/* low  nibble */
+#define NUM(minor)	((minor) & 0xf)		/* low nibble */
 
 
 /*
@@ -109,7 +113,7 @@ extern int scull_nr_devs;
 extern int scull_quantum;
 extern int scull_qset;
 
-extern int scull_p_buffer;	/* pipe.c */
+extern int scull_p_buffer;  /* pipe.c */
 
 
 /*
