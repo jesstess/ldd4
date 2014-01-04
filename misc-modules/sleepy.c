be347f24b3f19fdcd51c0b241920e6bee84bab8f
Jessica McKellar <jesstess@mit.edu>
2014-01-04 13:57:42 -0500

    misc-modules: clean up sleepy.

--- misc-modules/sleepy.c
+++ misc-modules/sleepy.c
@@ -12,26 +12,28 @@
  * by O'Reilly & Associates.   No warranty is attached;
  * we cannot take responsibility for errors or fitness for use.
  *
- * $Id: sleepy.c,v 1.7 2004/09/26 07:02:43 gregkh Exp $
  */
 
 #include <linux/module.h>
 #include <linux/init.h>
 
-#include <linux/sched.h>  /* current and everything */
-#include <linux/kernel.h> /* printk() */
-#include <linux/fs.h>     /* everything... */
-#include <linux/types.h>  /* size_t */
+#include <linux/sched.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/cdev.h>
 #include <linux/wait.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int sleepy_major = 0;
+static int sleepy_minor = 0;
 
 static DECLARE_WAIT_QUEUE_HEAD(wq);
 static int flag = 0;
 
-ssize_t sleepy_read (struct file *filp, char __user *buf, size_t count, loff_t *pos)
+ssize_t sleepy_read(struct file *filp, char __user *buf, size_t count,
+		loff_t *pos)
 {
 	printk(KERN_DEBUG "process %i (%s) going to sleep\n",
 			current->pid, current->comm);
@@ -41,16 +43,15 @@ ssize_t sleepy_read (struct file *filp, char __user *buf, size_t count, loff_t *
 	return 0; /* EOF */
 }
 
-ssize_t sleepy_write (struct file *filp, const char __user *buf, size_t count,
+ssize_t sleepy_write(struct file *filp, const char __user *buf, size_t count,
 		loff_t *pos)
 {
 	printk(KERN_DEBUG "process %i (%s) awakening the readers...\n",
 			current->pid, current->comm);
 	flag = 1;
 	wake_up_interruptible(&wq);
-	return count; /* succeed, to avoid retrial */
+	return count; /* Succeed, to avoid retrial. */
 }
-
 
 struct file_operations sleepy_fops = {
 	.owner = THIS_MODULE,
@@ -58,27 +59,34 @@ struct file_operations sleepy_fops = {
 	.write = sleepy_write,
 };
 
-
-int sleepy_init(void)
+static int __init sleepy_init(void)
 {
 	int result;
+	dev_t dev;
+	struct cdev *sleepy_cdev;
 
-	/*
-	 * Register your major, and accept a dynamic number
-	 */
-	result = register_chrdev(sleepy_major, "sleepy", &sleepy_fops);
-	if (result < 0)
+	/* Register dynamically. */
+	result = alloc_chrdev_region(&dev, sleepy_minor, 1, "sleepy");
+
+	if (result < 0) {
+		printk(KERN_WARNING "sleepy: couldn't get major number assignment\n");
 		return result;
-	if (sleepy_major == 0)
-		sleepy_major = result; /* dynamic */
+	}
+
+	sleepy_major = MAJOR(dev);
+
+	sleepy_cdev = cdev_alloc();
+	sleepy_cdev->ops = &sleepy_fops;
+	cdev_add(sleepy_cdev, dev, 1);
+
 	return 0;
 }
 
-void sleepy_cleanup(void)
+static void __exit sleepy_cleanup(void)
 {
-	unregister_chrdev(sleepy_major, "sleepy");
+	dev_t dev = MKDEV(sleepy_major, sleepy_minor);
+	unregister_chrdev_region(dev, 1);
 }
 
 module_init(sleepy_init);
 module_exit(sleepy_cleanup);
-
